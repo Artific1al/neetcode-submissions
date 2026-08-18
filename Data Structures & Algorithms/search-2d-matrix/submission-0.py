eb4b0fae4064047fc90734ee2be0bf689d2fce4a
Artific1al <96236536+Artific1al@users.noreply.github.com>
2026-08-19 07:43:16 +1000

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        l = 0
+        r = len(matrix[0]) * len(matrix) - 1
+        mid = r-l // 2 
+        
+        while l <=r:
+            column = mid // len(matrix[0]) #what bracket in the matrix
+            #row = r-l % len(matrix[0]) #what section of the bracket
+            mid_pos = mid % len(matrix[0])
+
+            #target found
+            if matrix[column][mid_pos] == target:
+                return True
+
+            #
+            elif matrix[column][mid_pos] > target:
+                r = mid - 1
+                mid = r - l // 2
+
+            #bigger than
+            else:
+                l = mid + 1
+                mid = r - l // 2
+        
+        return False
+
+#constraints
+
+#edge
+
+#brute force
+#O(m * n) = linear search
+
+#idea
+#-> binary search but mid has both a column and row
+#O(log n) is cause you keep halving the n sized array
+#O(log (m*n)) is imagine you stack all the columns side by side in one big long array as long as you
+#keep cutting it up and discarding one half you get m*n
+
+
